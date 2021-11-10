e05a5faf6a0d456306fd941ba33d811a6a97f969
shanjathurshan <jathurshanm3@gmail.com>
2021-11-10 10:30:49 +0530

    CRUD ADDED

---
+++ app/routes/customer.routes.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    const customers = require("../controllers/customer.controller.js");
+  
+    // Create a new Customer
+    app.post("/customers", customers.create);
+  
+    // Retrieve all Customers
+    app.get("/customers", customers.findAll);
+  
+    // Retrieve a single Customer with customerId
+    app.get("/customers/:customerId", customers.findOne);
+
+    // Search Customer with name
+    app.get("/customerss/:customerName", customers.searchByName);
+  
+    // Update a Customer with customerId
+    app.put("/customers/:customerId", customers.update);
+  
+    // Delete a Customer with customerId
+    app.delete("/customers/:customerId", customers.delete);
+  
+    // Create a new Customer
+    app.delete("/customers", customers.deleteAll);
+  };
